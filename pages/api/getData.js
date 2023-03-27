fa11fa628747def7e2c086cd02a3efe9ccada2f9
Mohamed Shakir <mshak084@uottawa.ca>
2023-03-26 20:13:12 -0400

    minor changes

---
+++ pages/api/getData.js
@@ -0,0 +1,25 @@
+import mysql from "mysql2/promise"
+require('dotenv').config()
+
+
+export default async function handler(req, res) {
+
+    const connection = await mysql.createConnection(process.env.DATABASE_URL);
+    
+    try{
+        
+        const query = "SELECT * FROM Hotel"
+
+        const values = []  
+
+        const [results] = await (await connection).execute(query, values)  
+        
+        connection.end()
+        
+        res.status(200).json({results: results})
+
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+    
+}
